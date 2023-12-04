18cc8d3475393cb1388ce13888ab0520621192ba
Adam Sinnett <quandrum@gmail.com>
2023-12-03 17:04:42 -0800

    Use AOC Runner

---
+++ src/day_12.js
@@ -0,0 +1,26 @@
+/**
+ * Contains solutions for Day 12
+ * Puzzle Description: https://adventofcode.com/2023/day/12
+ */
+
+/**
+ * Returns the solution for level one of this puzzle.
+ * @param {Object} args - Provides both raw and split input.
+ * @param {String} args.input - The original, unparsed input string.
+ * @param {String[]} args.lines - Array containing each line of the input string.
+ * @returns {Number|String}
+ */
+export const levelOne = ({ input, lines }) => {
+  // your code here
+};
+
+/**
+ * Returns the solution for level two of this puzzle.
+ * @param {Object} args - Provides both raw and split input.
+ * @param {String} args.input - The original, unparsed input string.
+ * @param {String[]} args.lines - Array containing each line of the input string.
+ * @returns {Number|String}
+ */
+export const levelTwo = ({ input, lines }) => {
+  // your code here
+};
